edbe4801735772d7907b422258ccf754d63ee5ed
uberj <jaxbr503@gmail.com>
2014-07-29 23:41:17 -0400

    add more to install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo 'works!' > /tmp/dpaste.out
+
+# XXX This is a total hack. We need to have apache do this be default
+grep '. /etc/environment' /etc/apache2/envvars
+
+if [ $? -eq 1 ]; then
+    echo '. /etc/environment' >> /etc/apache2/envvars
+fi
+
+SCRIPT_DIR=$(dirname $0)
+popd $SCRIPT_DIR
+pip install -r requirements.txt
+python manage.py syncdb --migrate

38842775631d3adc97a9418a260f332383fc7134
uberj <jaxbr503@gmail.com>
2014-07-30 12:56:30 -0400

    more hacks

--- install.sh
+++ install.sh
@@ -12,5 +12,8 @@ SCRIPT_DIR=$(dirname $0)
 pushd $SCRIPT_DIR
     pip install -r requirements.txt
     cp dpaste/settings/local.py.heat dpaste/settings/local.py
+    . /etc/environment
     python manage.py syncdb --migrate
 popd
+
+service apache2 restart
